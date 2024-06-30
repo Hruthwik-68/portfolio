a3c0fc0afb1ac5a82c39f2cbf2c58ce74446abe4
Hruthwik-68 <hruthwik6@gmail.com>
2024-06-30 14:53:05 +0530

    Updated files

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
           }
         },
         "color": {
-          "value": "#000000"
+          "value": "#fff"
         },
         "shape": {
           "type": "circle",
